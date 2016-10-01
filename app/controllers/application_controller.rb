0e876d132931592852201aa30eaf436581c0cde5
manuel <manutazo4@hotmail.es>
2016-10-01 22:04:22 +0200

    edit and create views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,9 @@ class ApplicationController < ActionController::Base
 
 
   protected
-     def configure_permitted_parameters
-        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :first_name, :last_name, :birthday, :description)}
-        devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password,:first_name, :last_name, :birthday, :description,  :avatar)}
-    end
+    def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :first_name, :last_name, :birthday, :description])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :current_password,:first_name, :last_name, :birthday, :description,  :avatar])
+  end
 
 end
