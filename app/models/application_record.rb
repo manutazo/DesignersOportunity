396d334a2d46fad5b055767f1d2c517fae5be63e
manuel <manutazo4@hotmail.es>
2016-10-01 16:59:04 +0200

    create views devise

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def index
+  end
+
+  protected
+     def configure_permitted_parameters
+        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password)}
+        devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
+    end
+
 end
