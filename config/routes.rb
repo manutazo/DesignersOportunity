bcc0233b630b0cc5b9949c2906438bb39067faf0
manuel <manutazo4@hotmail.es>
2016-10-01 22:14:52 +0200

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
+  
   devise_for :users
 
   get '/' => 'application#index'
+  get '/account' => 'users#show', as: 'account'
+  get 'designs' => 'designs#designs', as: 'designs'
+  get 'designer/:id' => 'designs#designer', as: 'designer'
+  post 'designs' => 'designs#create'
+
+  get 'designs/gender' => 'designs#filter_gender'
+  get 'designs/:gender' => 'designs#filter_season'
+
+    get 'designs/:gender/:season' => 'designs#filter', as: 'filter'
 
   mount Commontator::Engine => '/commontator'
+
+  delete 'design/:id/delete' => "designs#destroy", as: 'destroy_design'
+
 
   resources :designs , path: 'design', except: [:index]
   put	'/design/:id/like' =>	'designs#upvote', as: 'like'
