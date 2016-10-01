7072fc1541d56ed2c5f6c3cfc893f8c0eb68e185
manuel <manutazo4@hotmail.es>
2016-10-01 21:27:34 +0200

    add image designs and avatar image to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+  has_attached_file :avatar, :styles =>
+         { :medium => "300x300>", :thumb => "100x100#" }
+  validates_attachment_content_type :avatar, :content_type =>
+         /\Aimage\/.*\Z/
+  has_attached_file :image, styles:
+        { medium: "450x450>", thumb: "490x275>" }
+  validates_attachment_file_name :image, matches: [/png\z/, /jpg\z/, /jpe?g\z/]
+  validates_with AttachmentSizeValidator, attributes: :image, less_than: 1.megabytes
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161001150321) do
+ActiveRecord::Schema.define(version: 20161001192408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "designs", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "title",       null: false
-    t.text     "description", null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "gender",      null: false
-    t.string   "season",      null: false
+    t.string   "title",              null: false
+    t.text     "description",        null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "gender",             null: false
+    t.string   "season",             null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_designs_on_user_id", using: :btree
   end
 
@@ -42,6 +46,10 @@ ActiveRecord::Schema.define(version: 20161001150321) do
     t.string   "first_name"
     t.string   "last_name"
     t.date     "birthday"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
