604678bf718db8851cb500e1e14c1c0d1bccd5f3
manuel <manutazo4@hotmail.es>
2016-10-01 21:49:41 +0200

    add validates in models

--- app/models/design.rb
+++ app/models/design.rb
@@ -1,10 +1,19 @@
 class Design < ApplicationRecord
-  acts_as_votable
-  belongs_to :user
-  validates :title, presence: true, uniqueness: true, length: {maximum: 30, minimum: 4}
-  validates :description, presence: true, length: {maximum: 120, minimum: 5}, uniqueness: true
-  validates :image, presence: true
-  validates :gender, presence: true
-  validates :season, presence: true
-  acts_as_commontable
+    acts_as_votable
+    belongs_to :user
+    validates :title, presence: true,
+      uniqueness: true, length: {maximum: 30, minimum: 4}
+    validates :description, presence: true,
+     length: {maximum: 120, minimum: 5}, uniqueness: true
+    validates :image, presence: true
+    validates :gender, presence: true
+    validates :season, presence: true
+    has_attached_file :image, styles:
+      { medium: "450x450>", thumb: "490x275>" }
+    validates_attachment_file_name :image,
+      matches: [/png\z/, /jpg\z/, /jpe?g\z/]
+    validates_with AttachmentSizeValidator,
+      attributes: :image, less_than: 1.megabytes
+    acts_as_commontable
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,37 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  has_attached_file :avatar, :styles =>
-         { :medium => "300x300>", :thumb => "100x100#" }
-  validates_attachment_content_type :avatar, :content_type =>
-         /\Aimage\/.*\Z/
-  has_attached_file :image, styles:
-        { medium: "450x450>", thumb: "490x275>" }
-  validates_attachment_file_name :image, matches: [/png\z/, /jpg\z/, /jpe?g\z/]
-  validates_with AttachmentSizeValidator, attributes: :image, less_than: 1.megabytes
+      :recoverable, :rememberable, :trackable
+  validates_uniqueness_of :email,
+      :case_sensitive => false, :allow_blank => true, :if => :email_changed?
+  validates_format_of :email,
+      :with  => Devise.email_regexp, :allow_blank => true, :if => :email_changed?
+  validates_presence_of :password, :on=>:create
+  validates_confirmation_of :password, :on=>:create
+  validates_length_of :password,
+      :within => Devise.password_length, :allow_blank => true
+  validates_uniqueness_of :name,
+      :case_sensitive => false, :allow_blank => true, :if => :name_changed?, length: {maximum: 11, minimum: 3}
+  validates_presence_of :birthday, :on=>:create
+  validates_presence_of :first_name,
+      :on=>:create, length: {maximum: 11, minimum: 3}
+  validates_presence_of :last_name,
+      :on=>:create, length: {maximum: 11, minimum: 3}
+  validates_presence_of :description,
+      :on=>:create, length: {maximum: 60, minimum: 3}
+  has_many :designs, dependent: :destroy
+      has_attached_file :avatar, :styles =>
+  { :medium => "300x300>", :thumb => "100x100#" }
+      validates_attachment_content_type :avatar, :content_type =>
+  /\Aimage\/.*\Z/
   acts_as_commontator
+
+  def self.birth_date_first
+    118.years.ago
+  end
+
+  def self.birth_date_last
+    5.years.ago
+  end
+
 end
