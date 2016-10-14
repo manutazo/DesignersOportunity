698cd45bffb9218b2a6ba954f9c49357bb4d4a28
manuel <manutazo4@hotmail.es>
2016-10-14 06:24:54 +0200

    custom errors and remove current password of edit user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,8 @@ class ApplicationController < ActionController::Base
 
   protected
     def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :first_name, :last_name, :birthday, :description, :confirmation_token])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :first_name, :last_name, :birthday, :description,  :avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :first_name, :last_name, :birthday, :confirmation_token])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :first_name, :last_name, :birthday, :description, :avatar])
   end
 
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,6 +14,7 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'manutazo4@gmail.com'
 
+
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
