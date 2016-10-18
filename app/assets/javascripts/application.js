5aa6d7bc2ce86250bb7c64910d5ddb93bbbc736b
manuel <manutazo4@hotmail.es>
2016-10-18 02:01:34 +0200

    edit navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,14 +17,17 @@
 //= require turbolinks
 //= require_tree .
 
-  $("#flash_notice").delay(5000).fadeOut("slow");
-   $("#flash_alert").delay(5000).fadeOut("slow");
-   $("#flash_error").delay(5000).fadeOut("slow");
+$(document).ready(function() {
+  $('select').material_select();
+});
+
+$("#flash_notice").delay(5000).fadeOut("slow");
+$("#flash_alert").delay(5000).fadeOut("slow");
+$("#flash_error").delay(5000).fadeOut("slow");
 
 $(document).ready(function(){
     $('.tooltipped').tooltip({delay: 50});
   });
-
 
 $(document).ready(function(){
       $('.parallax').parallax();
@@ -34,11 +37,4 @@ $(document).ready(function() {
   $('input#input_text, textarea#textarea1').characterCounter();
 });
 
-$(document).ready(function(){
-    $('.modal-trigger').leanModal();
-});
-
-// Show sideNav
- $('.button-collapse').sideNav('show');
- // Hide sideNav
- $('.button-collapse').sideNav('hide');
+$(".button-collapse").sideNav();
